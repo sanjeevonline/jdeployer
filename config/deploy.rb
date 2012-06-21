6b60bda7d92301eb58204e59ec308c076fe7f321
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2012-06-21 15:27:16 +0530

    Building with Maven and Copying to tomcat/webapps works

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,13 @@
 server "localhost", :web, :app, :db, primary: true
 
-set :application, "jdeployer"
 set :user, "rockyj"
+set :application, "jdeployer"
+
+set :java_home, "/home/#{user}/jdk1.7.0_04"
+set :tomcat_home, "/home/#{user}/Apps/apache-tomcat-7.0.23"
+set :maven_home, "/home/#{user}/Apps/apache-maven-3.0.3"
 set :deploy_to, "/home/#{user}/Temp/#{application}"
+
 #set :deploy_via, :remote_cache
 set :use_sudo, false
 
@@ -14,11 +19,18 @@ default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 namespace :tomcat do
-  task :build_with_maven do
-    puts "echo pwd"
-    run "pwd > /home/rockyj/delme"
+  task :build_deploy_restart do
+    puts "==================Building with Maven======================"
+    run "export JAVA_HOME=#{java_home} && cd #{deploy_to}/current && #{maven_home}/bin/mvn clean package"
+    puts "==================Stop Tomcat======================"
+    run "export JAVA_HOME=#{java_home} && #{tomcat_home}/bin/shutdown.sh"
+    puts "==================Copy war to Tomcat======================"
+    run "cp #{deploy_to}/current/target/#{application}*.war #{tomcat_home}/webapps/#{application}.war"
+    puts "==================Start Tomcat======================"
+    run "export JAVA_HOME=#{java_home} && #{tomcat_home}/bin/startup.sh"
+    puts "==========================================================="
   end
 end
 
-after "deploy", "tomcat:build_with_maven" # keep only the last 5 releases
-after "tomcat:build_with_maven", "deploy:cleanup" # keep only the last 5 releases
+after "deploy", "tomcat:build_deploy_restart" # keep only the last 5 releases
+after "tomcat:build_deploy_restart", "deploy:cleanup" # keep only the last 5 releases
--- readme.md
+++ readme.md
@@ -1,1 +1,17 @@
 Simple JavaEE6 app to test deployment with Capistrano
+
+#If you are a Java programmer
+
+1. Install Ruby using RVM or RBENV (Ruby 1.9.3 is preferred)
+2. Run "gem install capistrano"
+3. Go to your Java project root directory and run "capify"
+4. This will create the capistrano config file.
+5. Copy the contents of this project's config/deploy.rb to your config/deploy.rb
+6. Change the variables at the top of this deploy file according to your machine
+7. Run "cap deploy"
+
+__Note__
+
+So far only the Maven build and war copy to Tomcat works.
+
+We need to fix starting and stopping Tomcat from Capistrano.
